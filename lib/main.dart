39f6210706e216917593d3e6975a84d0f2da5a32
Miguel Alvarez Lesmes <alvarez.lesmes.miguel@gmail.com>
2023-07-04 15:48:47 +0800

    extraer MyHomePage a otro archivo

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:curso_iniciacion_flutter/counter_button.dart';
+import 'package:curso_iniciacion_flutter/my_home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -16,26 +16,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Curso Iniciación Flutter'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-
-  const MyHomePage({super.key, required this.title});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(title),
-      ),
-      body: const Center(
-        child: CounterButton(),
-      ),
+      home: const MyHomePage(title: 'Curso de iniciación flutter'),
     );
   }
 }
